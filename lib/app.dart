0674efc2abcc53551e601a04de9f7bba5dafa4e4
nich.otieno <nicholas.otieno@revca.tech>
2023-12-14 21:50:41 +0300

    Refactor main.dart and remove widget_test.dart

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:health_symptoms/core/themes/theme.dart';
+import 'package:health_symptoms/features/onboarding/presentation/views/onboarding_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Health Symptoms',
+        theme: AppTheme.themeData(Brightness.light),
+        darkTheme: AppTheme.themeData(Brightness.dark),
+        home: const OnboardingView());
+  }
+}
